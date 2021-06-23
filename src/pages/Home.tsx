8639b28cffeeee05c8e13a110fcf972dd0ef805b
Gustavo Castilho Valenciano <gustavo.valenciano@credisis.com.br>
2021-06-23 10:15:56 -0300

    create context authenticated and hook useAuth

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,10 +7,17 @@ import googleIconImg from "../assets/images/google-icon.svg";
 import "../styles/auth.scss";
 import { Button } from "../components/Button";
 
+import { useAuth } from '../hooks/useAuth'
+
 export function Home() {
   const history = useHistory();
+  const { signInWithGoogle, user } = useAuth();
 
-  function navigateToNewRoom() {
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
+
     history.push('/rooms/new');
   }
 
@@ -27,7 +34,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button className="create-room" onClick={navigateToNewRoom}>
+          <button className="create-room" onClick={handleCreateRoom}>
             <img src={googleIconImg} alt="Logo do google" />
             Crie sua sala com o Google
           </button>
